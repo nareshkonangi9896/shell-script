d30f82cce92b6981bbda3f41c185b79da9d6cce1
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-17 20:57:54 +0530

    second commit

---
+++ 3.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello World,  I am learning shell scripting"
+
+Person1=$1
+Person2=$2
+
+echo "$Person1 : How are you $Person2 "
+echo "$Person2 : I am fine $Person1, How are you"
+echo "$Person1 : I am good $Person2"
