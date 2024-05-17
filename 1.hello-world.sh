2b5df7afe7f69d60d98aee7e727b0eb381677ad8
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-17 20:53:11 +0530

    second commit

--- 1.hello-world.sh
+++ 1.hello-world.sh
@@ -2,8 +2,8 @@
 
 echo "Hello World,  I am learning shell scripting"
 
-Person1 = Ramesh
-Person2 = Suresh
+Person1=Ramesh
+Person2=Suresh
 
 echo "$Person1 : How are you $Person2 "
 echo "$Person2 : I am fine $Person1, How are you"
