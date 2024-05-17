8d673c2c72e1b1053f6a8d103c8bc6edc76d9d50
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-17 20:49:06 +0530

    second commit

---
+++ 1.hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello World,  I am learning shell scripting"
+
+echo "Ramesh : How are you Suresh "
+echo "Suresh : I am fine Ramesh, How are you"
+echo "Ramesh : I am good Suresh"
