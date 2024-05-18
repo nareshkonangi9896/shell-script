c5cb2bb2ac52b4c8bedf47a310b0e163caff41bb
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-18 23:52:50 +0530

    commited

---
+++ 8.install_mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID  -ne 0 ]
+then
+    echo "ERROR : Run this script with root access"
+    exit 1
+#else
+fi
+
+    yum install mysql -y
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql is error"
+else
+    echo "Installation of mysql is success"
+fi
