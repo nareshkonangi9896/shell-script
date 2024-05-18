601d4fcbd4b77ed73d3ed3c926e68d030452179b
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-19 00:07:38 +0530

    commited

---
+++ 9.functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo "Installation.....FAILURE"
+    exit 1
+else
+    echo "Installation.....SUCCESS"
+ fi
+}
+
+if [ $USERID  -ne 0 ]
+then
+    echo "ERROR : Run this script with root access"
+    exit 1
+#else
+fi
+
+yum install mysql -y
+VALIDATE
+
+yum install postfix -y
+VALIDATE
