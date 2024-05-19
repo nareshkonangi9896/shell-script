7566bec6b41fd218c2b6823428ebed5fcb270f45
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-19 11:59:27 +0530

    commited

--- 10.loops.sh
+++ 10.loops.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 DATE=$(date +%F)
 SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 echo "$LOGFILE"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 USERID=$(id -u)
-echo "USERID IS $USERID"
-for i in $@
-do
-if [ $USERID -eq 0 ]
-then
-    yum install $i -y &>> $LOGFILE
-    if [ $? -eq 0 ]
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
     then
         echo -e $i "Installation$G SUCCESS $N"
     else
         echo -e $i "Installation$R FAILURE $N"
         exit 1
-    fi     
+    fi
+}
+
+EXIST_VALIDATE(){
+    yum list installed|grep $i &>> $LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e $i "Not Installed previously$G PROCEED TO INSTALL $N"
+    else
+        echo -e $i "Already Installed$R ABORT THE INSTALLATION $N"
+        exit 1
+    fi
+}
+
+for i in $@
+do
+if [ $USERID -eq 0 ]
+then
+    EXIST_VALIDATE $i
+    yum install $i -y &>> $LOGFILE  
+    VALIDATE $?
 else
     echo "Run this script as ROOT user"
     exit 1       
