caa6f93aaa341ebd7e3c3ce4ff6e9821d00d6b7a
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-19 10:16:51 +0530

    commited

--- 10.loops.sh
+++ 10.loops.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+DATE=$(date)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m]"
+USERID=$(id -u)
 for i in $@
 do
-    yum install $i -y
+if [ $USERID -e 0 ]
+then
+    yum install $i -y &>> $LOGFILE
+    echo -e $i "Installation $G SUCCESS $N"
+else
+    echo -e $i "Installation $R FAILURE $N"
+    exit 1       
+fi
 done
