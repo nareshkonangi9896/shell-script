6d0fa5239ae7789570fef0f91fdf854f6a7d8cb5
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-20 09:38:34 +0530

    commited

--- 11.DeleteLogs.sh
+++ 11.DeleteLogs.sh
@@ -5,11 +5,11 @@ SCRIPT_LOGS=/home/centos/script_logs
 echo "$SCRIPT_LOGS"
 SCRIPT_NAME=$0
 DATE=$(date +%F:%H:%M:%S)
-FILES_TO_DELETE=$(find APP_LOGS -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $APP_LOGS -name "*.log" -type f -mtime +14)
 
 while read line;
 do
     rm -rf $line
-    echo "Deleted $line" &>> SCRIPT_LOGS/SCRIPT_NAME-DATE.log
+    echo "Deleted $line" &>> $SCRIPT_LOGS/$SCRIPT_NAME-$DATE.log
 
 done <<< FILES_TO_DELETE
