df2b9849d2d86b32d21951cbf16bf54285041c80
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-20 09:27:30 +0530

    commited

--- 11.DeleteLogs.sh
+++ 11.DeleteLogs.sh
@@ -3,7 +3,7 @@ APP_LOGS= /home/centos/app_logs
 SCRIPT_LOGS=/home/centos/script_logs
 SCRIPT_NAME=$0
 DATE=$(date +%F:%H:%M:%S)
-FILES_TO_DELETE=find APP_LOGS -name "*.logs" -type f -mtime +14
+FILES_TO_DELETE=find APP_LOGS -name "*.log" -type f -mtime +14
 
 while read line;
 do
