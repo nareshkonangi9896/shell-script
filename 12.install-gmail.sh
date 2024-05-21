be1843895cdc9e89d2bc7f6b6ffc128151178a66
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-21 11:21:10 +0530

    commited

---
+++ 12.install-gmail.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo "Please enter Username"
+read -s USERNAME
+echo "Username entered is $USERNAME"
+echo "Please enter Password"
+read -s PASSWORD
+echo "Password entered is $PASSWORD"
+
+LOGDIR=/tmp/
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+
+R="\[31m"
+G="\[32m"
+N="\[0m"
+
+VALIDATE(){
+    if [ $1 -eq 0 ];
+    then
+        echo "$2 is $G SUCCESS $N"
+    else
+        echo "$2 is $R FAILURE $N"
+        exit 1
+    fi
+}
+
+ID=$(id -u)
+if [ ID -ne 0 ];
+then
+    echo -e "$R ERROR: Please run this script as ROOT user $N"
+    exit 1
+else
+    yum update -y --exclude=kernel* &>> $LOGFILE
+    VALIDATE $? updating yum
+
+    yum -y install postfix cyrus-sasl-plain mailx &>> $LOGFILE
+    VALIDATE $? installed postfix,cyrus-sasl-plain,mailx
+
+    systemctl restart postfix
+    VALIDATE $? restarted postfix &>> $LOGFILE
+
+    systemctl enable postfix 
+    VALIDATE $? enabled postfix &>> $LOGFILE
+
+    cat gmailconf.txt >> /etc/postfix/main.cf
+    VALIDATE $? copied gmail conf &>> $LOGFILE
+
+    touch /etc/postfix/sasl_passwd  
+    VALIDATE $? creating sasl_passwd file &>> $LOGFILE
+
+    sed -i "[smtp.gmail.com]:587 $USERNAME:$PASSWORD" /etc/postfix/sasl_passwd
+    VALIDATE $? Entered username and password &>> $LOGFILE
+
+    postmap /etc/postfix/sasl_passwd
+    VALIDATE $? postmap gmail and password &>> $LOGFILE 
+
+    echo "This is a test mail & Date $(date)" | mail -s "message" nareshkonangi9896@gmail.com &>> $LOGFILE
+     
+
+fi
