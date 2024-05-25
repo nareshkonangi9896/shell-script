808ff1f212070f1da1e5690531ea97057cd13884
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-25 18:57:41 +0530

    commited

--- diskusage.sh
+++ diskusage.sh
@@ -9,14 +9,15 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 USERID=$(id -u)
-DISKUSAGE=$(df -hT|grep -vE 'tmpfs|Filesystem'|awk -F " " '{print $6F}'|cut -d % -f 1)
-DISKNAME=$(df -hT|grep -vE 'tmpfs|Filesystem'|awk -F " " '{print $1F}')
+DISKUSAGE=$(df -hT|grep -vE 'tmpfs|Filesystem')
 MESSAGE=
 THRESHOLD=1
 if [ $USERID -eq 0 ]
 then
     while IFS= read line
     do
+    DISKUSAGE=$(echo $line|awk -F " " '{print $6F}'|cut -d % -f 1)
+    DISKNAME=$(echo $line|awk -F " " '{print $1F}')
         if [ $DISKUSAGE -gt $THRESHOLD ];
         then
             $MESSAGE+= "HIGH DISK USAGE ON $DISKNAME : $DISKUSAGE"
