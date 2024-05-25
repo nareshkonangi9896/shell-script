d02c2e118e7bbee5d569d5fc5774cedf044f19ea
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-25 20:04:10 +0530

    commited

--- mail.sh
+++ mail.sh
@@ -13,3 +13,8 @@ $ALERT_TYPE=$5
 #echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TOADDRESS"
 
 echo "all args : $@"
+
+FINAL_BODY=$(sed -e "s/TEAMNAME/$TEAMNAME/g" -e "s/ALERTYPE/$ALERT_TYPE/g" -e "s/Message/$BODY/g" gmail.html)
+
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TOADDRESS"
+
