81064f6294d9045f047e2bb4fc3b89cc10759989
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-25 18:52:32 +0530

    commited

--- diskusage.sh
+++ diskusage.sh
@@ -9,25 +9,25 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 USERID=$(id -u)
-DISKUAGE=$(df -hT|grep -vE 'tmpfs|Filesystem'|awk -F " " '{print $6F}'|cut -d % -f 1)
+DISKUSAGE=$(df -hT|grep -vE 'tmpfs|Filesystem'|awk -F " " '{print $6F}'|cut -d % -f 1)
 DISKNAME=$(df -hT|grep -vE 'tmpfs|Filesystem'|awk -F " " '{print $1F}')
-message=
+MESSAGE=
 THRESHOLD=1
 if [ $USERID -eq 0 ]
 then
     while IFS= read line
     do
-        if [ $DISKUAGE -gt $THRESHOLD ]
+        if [ $DISKUSAGE -gt $THRESHOLD ]
         then
-            $message+= "HIGH DISK USAGE ON $DISKNAME : $DISKUAGE"
+            $MESSAGE+= "HIGH DISK USAGE ON $DISKNAME : $DISKUSAGE"
         fi
-    done <<<$DISKUAGE
+    done <<<$DISKUSAGE
 else
     echo "Run this script as ROOT user"
     exit 1       
 fi
 
-echo "$message"
+echo "$MESSAGE"
 
 
 
