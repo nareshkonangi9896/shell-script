b6b133a1f948c45de97d655656dab0c7ef2078ef
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-25 19:58:51 +0530

    commited

--- mail.sh
+++ mail.sh
@@ -8,6 +8,6 @@ $ALERT_TYPE=$5
 
 #echo $BODY | mail -s $SUBJECT $TOADDRESS 
 
-FINAL_BODY=$(sed -e "s/TEAMNAME/$TEAMNAME/g" -e "s/ALERTYPE/$ALERT_TYPE/g" -e "s/Message/$BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TEAMNAME/$TEAMNAME/g" -e "s/ALERTYPE/$ALERT_TYPE/g" -e "s/Message/$BODY/g" gmail.html)
 
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TOADDRESS"
