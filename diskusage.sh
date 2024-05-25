68fa2da871de5773b521980a38fa3952d9c01eca
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-25 19:04:15 +0530

    commited

--- diskusage.sh
+++ diskusage.sh
@@ -20,7 +20,7 @@ then
     DISKNAME=$(echo $line|awk -F " " '{print $1F}')
         if [ $DISKUSAGE -gt $THRESHOLD ];
         then
-            MESSAGE+="HIGH DISK USAGE ON $DISKNAME : $DISKUSAGE\n"
+            MESSAGE+="HIGH DISK USAGE ON $DISKNAME : $DISKUSAGE \n"
         fi
     done <<<$DISKUSAGE
 else
@@ -28,7 +28,7 @@ else
     exit 1       
 fi
 
-echo $MESSAGE
+echo "message= $MESSAGE"
 
 
 
