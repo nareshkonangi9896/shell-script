dab557bb55a2ee21cb6818e778fcc6883447cd34
nareshkonangi9896 <nareshkonangi9896@gmail.com>
2024-05-25 11:18:21 +0530

    commited

---
+++ diskusage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILEDIR=/tmp
+LOGFILE=$LOGFILEDIR/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+DISKUAGE=$(df -hT|grep -vE 'tmpfs|Filesystem' #|awk -F " " '{print $6F}'|cut -d % -f 1)
+THRESHOLD=1
+if [ $USERID -eq 0 ]
+then
+    while IFS read line
+    do
+        echo "usage : $DISKUSAGE"
+
+    done
+else
+    echo "Run this script as ROOT user"
+    exit 1       
+fi
+
+
+
